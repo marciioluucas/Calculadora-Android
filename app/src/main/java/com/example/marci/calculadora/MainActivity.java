87c4c92954681bb33a632d7c7731285621a32ad9
Marcio Lucas <marciioluucas@gmail.com>
2017-08-23 16:53:29 -0300

    Fazendo metodo somar na MainActivity

--- app/src/main/java/com/example/marci/calculadora/MainActivity.java
+++ app/src/main/java/com/example/marci/calculadora/MainActivity.java
@@ -1,34 +1,36 @@
 package com.example.marci.calculadora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView display = (TextView) findViewById(R.id.display);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn0 = (Button) findViewById(R.id.button0);
-        Button btn1 = (Button) findViewById(R.id.button1);
-        Button btn2 = (Button) findViewById(R.id.button2);
-        Button btn3 = (Button) findViewById(R.id.button3);
-        Button btn4 = (Button) findViewById(R.id.button4);
-        Button btn5 = (Button) findViewById(R.id.button5);
-        Button btn6 = (Button) findViewById(R.id.button6);
-        Button btn7 = (Button) findViewById(R.id.button7);
-        Button btn8 = (Button) findViewById(R.id.button8);
-        Button btn9 = (Button) findViewById(R.id.button9);
-
-
+        final EditText valor1 = (EditText) findViewById(R.id.valor1);
+        final EditText valor2 = (EditText) findViewById(R.id.valor2);
         Button btnSoma = (Button) findViewById(R.id.soma);
         Button btnSubtrai = (Button) findViewById(R.id.subtrair);
         Button btnMultiplica = (Button) findViewById(R.id.multiplicar);
         Button btnDivide = (Button) findViewById(R.id.dividir);
-        Button btnLimpa = (Button) findViewById(R.id.C);
 
 
+
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                display.setText((int) new Calculadora(Float.parseFloat(valor1.getText().toString()), Float.parseFloat(valor2.getText().toString())).somar());
+            }
+        });
     }
+
 }
